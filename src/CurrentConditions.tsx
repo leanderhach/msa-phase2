bd5366e61d96b82eb259e43542d3df612af3e276
Leander Hach <leander.hach@gmail.com>
2022-07-29 17:17:44 +1200

    work on ui

--- src/CurrentConditions.tsx
+++ src/CurrentConditions.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import testData from './testData';
+import './CurrentConditions.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {IconProp} from "@fortawesome/fontawesome-svg-core";
+import Chip from '@mui/material/Chip';
 
-const currentLocation = "Auckland";
+const currentLocation = "Times Square, New York";
 const weatherData = testData.data.timelines[0].intervals;
+const currentTemperature = weatherData[0].values.temperature;
 
 const currentWeatherIcon = (): IconProp => {
     let weatherCondition = weatherData[0].values.weatherCode
@@ -34,19 +37,32 @@ const currentWeatherIcon = (): IconProp => {
     return weatherIcon;
 };
 
+function convertTemperature(convertTo: string, value: number) {
+    if(convertTo === "celsius") {
+        return Math.round(((value - 32) / 1.8));
+    }
+
+    return Math.round(value);
+}
+
 
 function CurrentConditions() {
     return (
         <div className="current-conditions">
             <div className="container">
-                <div className="condition-details">
-                    <div className="container">
-                        <FontAwesomeIcon icon="location-dot"></FontAwesomeIcon>
-                        {currentLocation}
-                    </div>
-                </div>
+                <FontAwesomeIcon icon="location-dot"></FontAwesomeIcon>
+                {currentLocation}
             </div>
-            <FontAwesomeIcon icon={currentWeatherIcon()}></FontAwesomeIcon>
+            <div className="container">
+                <div className="condition-details">
+                    <div className="container" id="temperature">
+                        <h1 className="title">{convertTemperature('celsius', currentTemperature)}</h1>
+                        <FontAwesomeIcon id="degree-icon" icon="circle-dot"></FontAwesomeIcon>
+                    </div>
+                    <Chip label="Cloudy"/>
+                </div>
+                <FontAwesomeIcon id="condition-icon" icon={currentWeatherIcon()}></FontAwesomeIcon>
+            </div>
         </div>
     );
 }
